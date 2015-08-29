f2cbe066129dad82694cda73ed76643ea089c441
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-08-29 14:13:23 +0200

    Add jquery and bootstrap

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-var app = require('express')(),
-    server = require('http').createServer(app),
-    io = require('socket.io').listen(server),
-    ent = require('ent'),
-    fs = require('fs'),
-    path = require('path'),
-    exphbs = require('express-handlebars'),
-    layout = require('express-layout');
+var express = require('express'),
+    app     = express(),
+    server  = require('http').createServer(app),
+    io      = require('socket.io').listen(server),
+    ent     = require('ent'),
+    fs      = require('fs'),
+    path    = require('path'),
+    exphbs  = require('express-handlebars'),
+    layout  = require('express-layout');
 
-// app.engine('.hbs', exphbs({defaultLayout: 'layout', extname: '.hbs'}));
-// app.set('views', path.join(__dirname, 'views'));
 app.use(layout());
 
 app.set('layouts', './views/layouts');
 app.set('layout', 'default');
-
 app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 var indexController = require('./controllers/index');
 
