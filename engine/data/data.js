9b46e4f610f936b50d3832e4e8804df969e868cb
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-09-15 06:25:40 +0200

    Update Data engine

--- engine/data/data.js
+++ engine/data/data.js
@@ -1,34 +1,94 @@
+/*
+* size hitbox with xD
+* viewDistance with xD
+*
+*/
+
 var Data = {
   agent : {
     unit : {
       observer  : {
-        cost : ,
+        cost : {
+          plant :
+          mineral :
+        },
         health : ,
         armor : ,
         hitbox : {
-          width : ,
-          height :
-        }
+          width : 1,
+          height : 1
+        },
+        viewDistance : 6
       },
 
       builder   : {
-
+        cost : {
+          plant :
+          mineral :
+        },
+        health : ,
+        armor : ,
+        hitbox : {
+          width : 1,
+          height : 1
+        },
+        viewDistance : 3
       },
 
       soldier   : {
-
+        cost : {
+          plant :
+          mineral :
+        },
+        health : ,
+        armor : ,
+        hitbox : {
+          width : 1,
+          height : 1
+        },
+        viewDistance : 2
       },
 
       healer    : {
-
+        cost : {
+          plant :
+          mineral :
+        },
+        health : ,
+        armor : ,
+        hitbox : {
+          width : 1,
+          height : 1
+        },
+        viewDistance : 3
       },
 
       defender  : {
-
+        cost : {
+          plant :
+          mineral :
+        },
+        health : ,
+        armor : ,
+        hitbox : {
+          width : 1,
+          height : 1
+        },
+        viewDistance : 4
       },
 
       picker    : {
-
+        cost : {
+          plant :
+          mineral :
+        },
+        health : ,
+        armor : ,
+        hitbox : {
+          width : 1,
+          height : 1
+        },
+        viewDistance : 2
       }
     },
 
@@ -37,7 +97,26 @@ var Data = {
     },
 
     resource : {
+      plant : {
+        hitbox : {
+          width : 0.5
+          height : 0.5
+        }
+      },
 
+      mineral : {
+        hitbox : {
+          width : 0.5
+          height : 0.5
+        }
+      },
+
+      darkmatter : {
+        hitbox : {
+          width : 0.5
+          height : 0.5
+        }
+      },
     },
 
     deposit  : {
