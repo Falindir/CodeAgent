a54cedf51685c7ab9cadf52eca5fb13401ad6b62
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-09-21 21:20:02 +0200

    Creation team + list team user create

--- controllers/teamsController.js
+++ controllers/teamsController.js
@@ -1,15 +1,49 @@
 var teams = require('express').Router();
 var mongoose = require('mongoose');
-
+var rankUser = require('../tools/rank.js');
 var User = mongoose.model('User');
+var Team = mongoose.model('Team');
 
 teams.get('/', function(req, res) {
   if(req.user !== undefined) {
-    res.render('partials/teams', {
-        title : 'Code Agent - Teams',
-        isTeamsPage : true,
-        user : req.user
+
+    var teams;
+
+    Team.find({creator: req.user._id}, function(err, t) {
+      res.render('partials/teams', {
+          title : 'Code Agent - Teams',
+          isTeamsPage : true,
+          user : req.user,
+          teamsUser : t
+      });
     });
+  }
+  else {
+    res.redirect('/');
+  }
+
+});
+
+teams.post('/new', function(req, res) {
+  if(req.user !== undefined) {
+
+    var name = req.body.name;
+    var user = req.user;
+    var description = req.body.description;
+
+    var team = new Team(
+    { name: name, description: description, elo: 1000,
+      rank: rankUser.empty, creationDate: new Date(),
+      creator: user._id, members: [user._id]
+    });
+
+    team.save(function (err) {
+        if (err) {
+            next(err);
+        }
+    });
+
+    res.redirect('/');
   }
   else {
     res.redirect('/');
