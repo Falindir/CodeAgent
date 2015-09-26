3a9b5ab700962564f16362e13c7cd1bb8c2d5d20
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-09-26 11:40:36 +0200

    Update lib LILI & Sprite with LIXI

--- public/script/lib/lili.js
+++ public/script/lib/lili.js
@@ -77,6 +77,280 @@
       return Class;
     };
 
+
+    LILI.isUndefined = function(val) {
+      return typeof(val) === "undefined";
+    };
+
+    LILI.Collections = {};
+
+    LILI.Collections.List = Class.extend({
+
+      init : function() {
+        this.collections = [];
+        this.size = 0;
+      },
+
+      clear : function () {
+        this.collections = [];
+        this.size = 0;
+      },
+
+      add : function (value) {
+        this.collections.push(value);
+        this.size += 1;
+      },
+
+      get : function (index) {
+        return this.collections[index];
+      },
+
+      last : function () {
+        if(this.collections.length === 0)
+          return undefined;
+        else
+          return this.collections[this.collections.length - 1];
+      },
+
+      set : function (index, value) {
+        this.collections[index] = value;
+      },
+
+      remove : function (index) {
+        this.collections.splice(index, 1);
+        if(this.size > 0)
+          this.size -= 1;
+      },
+
+      contains : function (value) {
+        for (i = 0; i < this.collections.length; i++)
+          if(this.collections[i] === value)
+            return true;
+
+        return false;
+      },
+
+      getFirstIndex : function (value) {
+        for (i = 0; i < this.collections.length; i++)
+          if(this.collections[i] === value)
+            return i;
+
+        return undefined;
+      },
+
+      getAllIndex : function (value) {
+        var index = LILI.Collections.List.create();
+
+        for (i = 0; i < this.collections.length; i++)
+          if(this.collections[i] === value)
+            index.add(i);
+
+        return index;
+
+      },
+
+      getLastIndex : function (value) {
+        var listVal = this.getAllIndex(value);
+
+        if(listVal.size === 0)
+          return undefined;
+
+        return listVal.last();
+      },
+
+      getInterval : function (start, end) {
+        var interval = LILI.Collections.List.create();
+
+        for (i = start; i <= end; i++) {
+          interval.add(this.get(i));
+        }
+
+        return interval;
+      },
+
+      addTab : function (tab, reset) {
+        if(reset)
+          this.clear();
+
+        for(var i = 0; i < tab.length; i++)
+          this.add(tab[i]);
+      },
+
+      addList : function (list, reset) {
+        if(reset)
+          this.clear();
+
+        for(var i = 0; i < list.size; i++)
+          this.add(list.get(i));
+      }
+    });
+
+    LILI.Collections.Map = Class.extend({
+
+      init : function() {
+        this.keys = LILI.Collections.List.create();
+        this.values = LILI.Collections.List.create();
+        this.size = 0;
+      },
+
+      clear : function() {
+        this.keys.clear();
+        this.values.clear();
+        this.size = 0;
+      },
+
+      insert : function (key, value) {
+        if(!this.keys.contains(key)) {
+          this.keys.add(key);
+          this.values.add(value);
+          this.size += 1;
+        }
+      },
+
+      get : function (key) {
+        if(this.keys.contains(key)) {
+          return this.values.get(this.keys.getFirstIndex(key));
+        }
+
+        return null;
+      },
+
+      getContent : function (index) {
+
+        if(index < this.size) {
+          return this.values.get(index);
+        }
+
+        return null;
+
+      },
+
+      remove : function (key) {
+        if(this.keys.contains(key)) {
+          var index = this.keys.getIndex(key);
+          this.keys.remove(index);
+          this.values.remove(index);
+          this.size -= 1;
+        }
+      },
+
+      contains : function (key) {
+        return this.keys.contains(key);
+      },
+
+      getNumberOfKey : function(key) {
+        var n = 0;
+
+        for (var i = 0; i < this.keys.size; i++) {
+          if(this.keys.get(i) === key) {
+            n++;
+          }
+        }
+
+        return n;
+      },
+
+      getAllIndex : function(key) {
+        return this.keys.getAllIndex(key);
+      }
+    });
+
+    LILI.Math = {};
+
+    LILI.Math.random = function (low, high, round) {
+      if(round)
+        return Math.round(Math.random() * (high - low) + low);
+
+      return Math.random() * (high - low) + low;
+    };
+
+    LILI.Math.radians = function(degrees) {
+      return degrees * Math.PI / 180;
+    };
+
+    // Converts from radians to degrees.
+    LILI.Math.degrees = function(radians) {
+      return radians * 180 / Math.PI;
+    };
+
+    LILI.Math.percentageCalculator = function(number, percent) {
+      return number + (number * percent) / 100;
+    };
+
+    LILI.Geometric = {};
+
+    LILI.Geometric.generatePoint2DInCircleZone = function(origin, radius, minDist) {
+
+      var angle = Math.random()*Math.PI * 2;
+
+      var px = Math.cos(angle)*(radius - minDist) + minDist + origin.x;
+      var py = Math.sin(angle)*(radius - minDist) + minDist + origin.y;
+
+      var p = LILI.Geometric.Point2D.create(Math.round(px), Math.round(py));
+
+      if(p.distance(origin) < minDist) // securiy
+        return LILI.Geometric.generatePointInCircleZone(origin, radius, minDist);
+
+      return p;
+    };
+
+    //TODO NEED REFACTOR;
+    LILI.Geometric.rotationPoint2D = function(point, angle) {
+      var x = point.x*Math.cos(angle) - point.y*Math.sin(angle);
+      var y = point.x*Math.sin(angle) + point.y*Math.cos(angle);
+      return LILI.Geometric.Point2D.create(x, y);
+    };
+
+    LILI.Geometric.translationPoint2D = function(point, angle, distance) {
+      point.x += distance * Math.cos(angle);
+      point.y += distance * Math.sin(angle);
+    };
+
+    LILI.Geometric.Point2D = Class.extend({
+
+      init : function(x, y) {
+        this.x = x;
+        this.y = y;
+      },
+
+      haveSameX : function (p2) {
+        return this.x === p2.x;
+      },
+
+      haveSameY : function (p2) {
+        return this.y === p2.y;
+      },
+
+      isSame : function (p2) {
+        return this.haveSameX(p2) && this.haveSameY(p2);
+      },
+
+      set : function (p2) {
+        this.x = p2.x;
+        this.y = p2.y;
+      },
+
+      setLocation : function (x, y) {
+        this.x = x;
+        this.y = y;
+      },
+
+      distance : function (p2) {
+        return Math.sqrt(Math.pow(p2.x - this.x) + Math.pow(p2.y - this.y));
+      },
+
+      round : function(preci) {
+        var r = Math.pow(10, preci);
+        this.x = Math.round(this.x * r) / r;
+        this.y = Math.round(this.y * r) / r;
+      },
+
+      toString : function() {
+        return "Point : x = " + this.x + ", y = " + this.y + ";";
+      }
+
+    });
+
     LILI.sayHello();
 
     window.LILI = LILI;
--- public/script/lib/lixi.js
+++ public/script/lib/lixi.js
@@ -11,6 +11,11 @@
 * @author Lopez Jimmy https://github.com/Falindir
 */
 
+/**
+* need lili.js
+* need pixi.js
+**/
+
 ;(function() {
 
   var LIXI = LIXI || {};
@@ -22,6 +27,121 @@
   LIXI.sayHello = function () {
     console.log('Lixi.ls : ' + LIXI.VERSION + " - author : " + LIXI.AUTHOR);
   };
+
+  LIXI.Cursor = {
+    default : 'default',
+    pointer  : 'pointer'
+  };
+
+  LIXI.Sprite = LILI.Class.extend({
+
+    init : function(texture) {
+      this.sprite               = new PIXI.Sprite(texture);
+      this.sprite.position.x    = 0;
+      this.sprite.position.y    = 0;
+      this.sprite.anchor.x      = 0;
+      this.sprite.anchor.y      = 0;
+      this.sprite.alpha         = 1;
+      this.sprite.scale.x       = 1;
+      this.sprite.scale.y       = 1;
+      this.sprite.rotation      = 0;
+      this.sprite.visible       = true;
+      this.sprite.interactive   = false;
+      this.sprite.buttonMode    = false;
+      this.sprite.defaultCursor = LIXI.Cursor.default;
+      this.sprite.zIndex        = 0;
+    },
+
+    setPosX : function (posX) {
+      this.sprite.position.x = posX;
+    },
+
+    setPosY : function (posY) {
+      this.sprite.position.y = posY;
+    },
+
+    setPos : function(posX, posY) {
+      this.sprite.setPosX(posX);
+      this.sprite.setPosY(posY);
+    },
+
+    multiplyPosFactor : function (factor) {
+      this.sprite.position.x *= factor;
+      this.sprite.position.y *= factor;
+    },
+
+    setAnchX : function (anchX) {
+      this.sprite.anchor.x = anchX;
+    },
+
+    setAnchY : function (anchY) {
+      this.sprite.anchor.y = anchY;
+    },
+
+    setAnchs : function (val) {
+      this.setAnchX(val);
+      this.setAnchY(val);
+    },
+
+    setAlpha : function (alpha) {
+      this.sprite.alpha = alpha;
+    },
+
+    setScaleX : function (scaleX) {
+      this.sprite.scale.x = scaleX;
+    },
+
+    setScaleY : function (scaleY) {
+      this.sprite.scale.y = scaleY;
+    },
+
+    setScales : function (val) {
+      this.setScaleX(val);
+      this.setScaleY(val);
+    },
+
+    multiplyScalesFactor : function (factor) {
+      this.sprite.scale.x *= factor;
+      this.sprite.scale.y *= factor;
+    },
+
+    setVisible : function (visible) {
+      this.sprite.visible = visible;
+    },
+
+    setRotation : function (rotation) {
+      this.sprite.rotation = rotation;
+    },
+
+    incrementRotation : function (rotation) {
+      this.sprite.rotation += rotation;
+    },
+
+    setInteractive : function (interact) {
+      this.sprite.interactive = interact;
+    },
+
+    setButtonMode : function (butMode) {
+      this.sprite.buttonMode = butMode;
+    },
+
+    setCursor : function (cursor) {
+      this.sprite.defaultCursor = cursor;
+    },
+
+    getX : function () {
+      return this.sprite.position.x;
+    },
+
+    getY : function () {
+      return this.sprite.position.y;
+    },
+
+    setTexture : function (texture) {
+      this.sprite.setTexture(texture);
+    }
+
+  });
 
   LIXI.sayHello();
 
