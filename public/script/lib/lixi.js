766196759cda8a943abbd47bcb4cf4d52b557c93
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-09-30 10:10:45 +0200

    Add Stream + fix bug

--- public/script/lib/lixi.js
+++ public/script/lib/lixi.js
@@ -37,9 +37,9 @@
 
   LIXI.Compare.zIndex = function (a,b) {
     if (a.zIndex < b.zIndex)
-      return -1;
+    return -1;
     if (a.zIndex > b.zIndex)
-      return 1;
+    return 1;
 
     return 0;
   };
@@ -189,27 +189,27 @@
   LIXI.Button = LIXI.Sprite.extend({
 
     init : function(name, texture) {
-    	this._super(texture);
-  		this.name   = name;
-  		this.type   = -1;
-  		this.isDown = false;
-  		this.setInteractive(true);
-  		this.setButtonMode(true);
-  		this.setCursor(LIXI.CURSOR.pointer);
+      this._super(texture);
+      this.name   = name;
+      this.type   = -1;
+      this.isDown = false;
+      this.setInteractive(true);
+      this.setButtonMode(true);
+      this.setCursor(LIXI.CURSOR.pointer);
     }
   });
 
   LIXI.Camera = LILI.Class.extend({
 
     init : function(stage) {
-    	this.container = new PIXI.Container();
-    	stage.addChild(this.container);
+      this.container = new PIXI.Container();
+      stage.addChild(this.container);
       this.container.position.x = 0;
       this.container.position.y = 0;
     },
 
     addSprite : function (sprite) {
-    	this.container.addChild(sprite);
+      this.container.addChild(sprite);
     },
 
     removeSprite : function(sprite) {
@@ -217,32 +217,32 @@
     },
 
     initPosition : function () {
-        this.setPos(0, 0);
+      this.setPos(0, 0);
     },
 
     setPosX : function (posX) {
-    	this.container.position.x = posX;
+      this.container.position.x = posX;
     },
 
     setPosY : function (posY) {
-    	this.container.position.y = posY;
+      this.container.position.y = posY;
     },
 
     setPos : function (posX, posY) {
-    	this.setPosX(posX);
-    	this.setPosY(posY);
+      this.setPosX(posX);
+      this.setPosY(posY);
     },
 
     samePosX : function (posX) {
-    	return this.container.position.x == posX;
+      return this.container.position.x == posX;
     },
 
     samePosY : function (posY) {
-    	return this.container.position.y == posY;
+      return this.container.position.y == posY;
     },
 
     samePos : function (posX, posY) {
-    	return this.samePosX(posX) && this.samePosY(posY);
+      return this.samePosX(posX) && this.samePosY(posY);
     },
 
     moveX : function (dx) {
@@ -258,8 +258,8 @@
   LIXI.Background = LILI.Class.extend({
 
     init : function(stage, texture, width, height, scale) {
-    	this.container = new PIXI.Container();
-    	this.tilingSprite = new PIXI.extras.TilingSprite(texture, width, height);
+      this.container = new PIXI.Container();
+      this.tilingSprite = new PIXI.extras.TilingSprite(texture, width, height);
       this.setScale(scale);
       stage.addChild(this.container);
       this.container.addChild(this.tilingSprite);
@@ -272,7 +272,104 @@
 
   });
 
-  
+  LIXI.Stream = LILI.Class.extend({
+
+    init : function(cnt, color, backgroundTexture) {
+      this.nameContainer = cnt;
+      this.container     = $(cnt)[0];
+      this.renderer      = new PIXI.autoDetectRenderer(this.container.offsetWidth,this.container.offsetHeight,{backgroundColor : color});
+      this.stage         = new PIXI.Container();
+      this.backGround    = new LIXI.Background(this.stage, backgroundTexture, 2000, 2000, 0.3);
+      this.camera        = new LIXI.Camera(this.stage);
+      this.hud           = new LIXI.Camera(this.stage);
+
+      this.coordCenterX  = 0;
+      this.coordCenterY  = 0;
+      this.zoom          = 1;
+      this.minZoom       = 0;
+      this.maxZoom       = 100000;
+      this.prevX         = 0;
+      this.prevY         = 0;
+      this.isDragging    = false;
+      this.activeZoom    = true;
+      this.activeMove    = true;
+    },
+
+    initStream : function() {
+      this.stage.interactive = true;
+      this.renderer.view.style.display = "block";
+      this.container.appendChild(this.renderer.view);
+      this.camera.initPosition();
+    },
+
+    resizeStream : function () {
+      this.renderer.resize(this.container.offsetWidth-1, this.container.offsetHeight-1);
+      this.coordCenterX = this.container.offsetWidth-1 / 2;
+      this.coordCenterY = this.container.offsetHeight-1 / 2;
+      this.backGround.tilingSprite.height = this.container.offsetHeight / 0.3;
+      this.backGround.tilingSprite.width = this.container.offsetWidth / 0.3;
+    },
+
+    renderStream : function () {
+      this.renderer.render(this.stage);
+    },
+
+    cameraMove : function () {
+
+      var self = this;
+
+      this.stage.mousedown = function (moveData) {
+        var pos = moveData.data.global;
+        this.prevX = pos.x;
+        this.prevY = pos.y;
+        this.isDragging = true;
+      };
+
+      this.stage.mouseup = function (moveDate) {
+        this.isDragging = false;
+      };
+
+      this.stage.mouseover = function (moveDate) {
+
+      };
+
+      this.stage.mousemove = function (moveData) {
+
+        if(this.activeMove) {
+          if (!this.isDragging) {
+            self.moveWhenNotDragging(moveData);
+            return;
+          }
+
+          var pos = moveData.data.global;
+
+          if(pos.x > 0 && pos.y > 0 && pos.x < self.container.offsetWidth-1 && pos.y < self.container.offsetHeight-1) {
+            var dx = pos.x - this.prevX;
+            var dy = pos.y - this.prevY;
+
+            //self.camera.position.x += dx;
+            //self.camera.position.y += dy;
+            self.camera.moveX(dx);
+            self.camera.moveY(dy);
+
+            this.prevX = pos.x;
+            this.prevY = pos.y;
+          }
+          else {
+            this.isDragging = false;
+          }
+        }
+        else {
+          self.moveWhenNotDragging(moveData);
+        }
+      };
+    },
+
+    moveWhenNotDragging : function (moveData) {
+      //abstract function
+    }
+
+  });
 
   LIXI.sayHello();
 
