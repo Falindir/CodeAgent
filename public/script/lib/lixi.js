763f5345fc79edea467be50a6e091a96e6541c4c
Lopez Jimmy <jimmy.lopez@etud.univ-montp2.fr>
2015-09-30 10:04:24 +0200

    Add LIXI.Camera & LIXI.Backgroung

--- public/script/lib/lixi.js
+++ public/script/lib/lixi.js
@@ -199,7 +199,80 @@
     }
   });
 
-  LIXI.Editor = {};
+  LIXI.Camera = LILI.Class.extend({
+
+    init : function(stage) {
+    	this.container = new PIXI.Container();
+    	stage.addChild(this.container);
+      this.container.position.x = 0;
+      this.container.position.y = 0;
+    },
+
+    addSprite : function (sprite) {
+    	this.container.addChild(sprite);
+    },
+
+    removeSprite : function(sprite) {
+      this.container.removeChild(sprite);
+    },
+
+    initPosition : function () {
+        this.setPos(0, 0);
+    },
+
+    setPosX : function (posX) {
+    	this.container.position.x = posX;
+    },
+
+    setPosY : function (posY) {
+    	this.container.position.y = posY;
+    },
+
+    setPos : function (posX, posY) {
+    	this.setPosX(posX);
+    	this.setPosY(posY);
+    },
+
+    samePosX : function (posX) {
+    	return this.container.position.x == posX;
+    },
+
+    samePosY : function (posY) {
+    	return this.container.position.y == posY;
+    },
+
+    samePos : function (posX, posY) {
+    	return this.samePosX(posX) && this.samePosY(posY);
+    },
+
+    moveX : function (dx) {
+      this.container.position.x += dx;
+    },
+
+    moveY : function (dy) {
+      this.container.position.y += dy;
+    }
+
+  });
+
+  LIXI.Background = LILI.Class.extend({
+
+    init : function(stage, texture, width, height, scale) {
+    	this.container = new PIXI.Container();
+    	this.tilingSprite = new PIXI.extras.TilingSprite(texture, width, height);
+      this.setScale(scale);
+      stage.addChild(this.container);
+      this.container.addChild(this.tilingSprite);
+    },
+
+    setScale : function (scale) {
+      this.tilingSprite.scale.x = scale;
+      this.tilingSprite.scale.y = scale;
+    }
+
+  });
+
+  
 
   LIXI.sayHello();
 
